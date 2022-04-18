109b98b7b60944ce20c78f248636423dfe559309
megiak <akmelmegfirah@gmail.cao>
2022-04-18 01:26:41 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h" 
+
+/** 
+ * * _puts - prints string 
+ * * @str: pointer pointing to the string 
+ * * Return: void 
+ * */
+void _puts(char *str)
+{	
+	char c;
+
+	while (*str)	
+	{		
+		c = *str++;		
+		_putchar(c);	
+	}	
+	_putchar('\n');
+}
