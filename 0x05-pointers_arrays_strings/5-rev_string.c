98b0c8e0525cb1737442dfe8026267a9b49eba17
megiak <akmelmegfirah@gmail.cao>
2022-04-18 03:53:59 -0700

    arey string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,42 @@
+ #include "main.h"
+
+/**
+ *  * rev_string - reverses a string
+ *   * @s: pointer of the string
+ *    * Return: void
+ *     */
+void rev_string(char *s)
+{
+		int front = 0, back;
+
+			back = _strlen(s) - 1;
+
+				while (back > front)
+						{
+									char temp;
+
+											temp = *(s + back);
+													*(s + back) = *(s + front);
+															*(s + front) = temp;
+																	back--;
+																			front++;
+																				}
+}
+
+/**
+ *  * _strlen - return the length of a a string
+ *   * @s: string to be checked
+ *    *
+ *     * Return: lenght of string
+ *      **/
+int _strlen(char *s)
+{
+		int var;
+			int output = 0;
+
+				for (var = 0; s[var] != '\0'; var++)
+						{
+									output += 1;
+										}
+					return (output);
+}
