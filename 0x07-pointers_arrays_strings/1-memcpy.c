bcf0a845522403a67509c7561dedd6aba8fbc269
megiak <akmelmegfirah@gmail.cao>
2022-04-25 04:22:48 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,7 +7,8 @@
  *     * @n: number of bytes to copy
  *      *
  *       * Return: pointer to dest
- *        */char *_memcpy(char *dest, char *src, unsigned int n)
+ *        */
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 		unsigned int i;
 
