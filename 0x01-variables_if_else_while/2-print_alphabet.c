85aa842261509eb7deddf8d861e711f2e1b26dd1
megiak <akmelmegfirah@gmail.com>
2022-07-01 12:02:57 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,7 +4,7 @@ nclude <stdio.h>
  * * followed by a new line
  * * Return: Always 0 (Success)
  * */int main(void)
-{ char ch;
+{char ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{putchar(ch); }
 	putchar('\n');
