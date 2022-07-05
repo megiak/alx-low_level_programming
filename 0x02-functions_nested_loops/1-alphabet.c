54fd291cc702fd177d8dc48b140285600b16f036
megiak <akmelmegfirah@gmail.com>
2022-07-05 07:34:56 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,15 +4,14 @@
  *  * print_alphabet - print alphabet
  *   *
  *    * Return: void
- *     */
-void print_alphabet(void)
+ *     */void print_alphabet(void)
 {
 		char letter = 'a';
 
-			while (letter <= 'z')
-					{
+			 (letter <= 'z')
+				 	{
 								_putchar(letter);
 										letter++;
 											}
-				_putchar('\n');
+			 	_putchar('\n');
 }
